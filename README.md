c7ae781f2123b74219adecd666f1cec35c7fc424
albert <albert.fang@tarokosoftware.com>
2017-11-15 22:56:27 +0800

    project basic setup

---
+++ README.md
@@ -0,0 +1,21 @@
+## OMG - Component
+
+## develop
+* install the packages by running
+
+```
+npm install
+```
+
+
+<!---
+Brief description
+How to get the project running locally
+Running the linter
+Running the test suite
+How to contribute
+Steps to submit a PR
+How to raise issues
+A link to the Code of Conduct
+A changelog
+-->
---
+++ webpack.dev.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    path.join(__dirname, 'src/index.js')
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    } , {
+      test: /\.scss$/,
+      loaders:["style-loader", "css-loader", "sass-loader"]
+    }]
+  },
+  devServer: {
+    contentBase: './',
+    historyApiFallback: true
+  }
+};
