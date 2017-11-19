e6ed50d77300aefa9fae7757a9aa8e936251d9e7
Albert Fang <hr00090241@gmail.com>
2017-11-19 16:27:49 +0800

    fix webpack resolve problem

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { SocialLogin } from 'components';
 
 export default class App extends Component {
   render () {
-    return <p>This is my new react app</p>;
+    return (
+      <div className="main-container">
+        <p>This is my new react app</p>
+        <SocialLogin />
+      </div>
+    )
   }
 }
